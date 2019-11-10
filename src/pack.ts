11298a97aa26fb8295171172b970a5af00ed80b4
mothepro <4e688d8adf9b8f68774900f9809778ccb7ef857a>
2019-11-09 18:57:05 -0800

    Add a SDP packer and utility classes

---
+++ src/pack.ts
@@ -0,0 +1,62 @@
+import { delimiter, bytesToStr } from './util'
+
+/**
+ * Pack a RTC Session Description to the minimal
+ * 
+ * SDP must be version 0.
+ * Only candidates with IPv4 are encoded.
+ * 
+ * The grammar can be found here
+ * @see https://tools.ietf.org/html/rfc5245#section-15.1
+ * 
+ * The explanation on how this works
+ * @see https://webrtchacks.com/the-minimum-viable-sdp/
+ */
+export default function ({ type, sdp }: RTCSessionDescription): string {
+  if (!sdp.startsWith('v=0'))
+    throw Error('Provided SDP is not version 0')
+  
+  let ufrag!: string,
+    pwd!: string,
+    fingerprint!: string,
+    candidates: string[] = []
+  
+  for (const line of sdp.split('\n')) {
+    const splitter = line.indexOf(':')
+    if (splitter <= 0) // must have a attribute and value pair
+      continue
+    
+    const [attribute, value] = [line.slice(0, splitter), line.slice(splitter + 1).trim()]
+    
+    switch (attribute) {
+      case 'a=ice-ufrag':
+        ufrag = value
+        break
+
+      case 'a=ice-pwd':
+        pwd = value
+        break
+
+      case 'a=fingerprint':
+        fingerprint = bytesToStr(value.substr('sha-256'.length).trim().split(':').map(byte => parseInt(byte, 16)))
+        break
+        
+      case 'a=candidate':
+        const [foundation, componentId, transport, priority, connectionAddress, port] = line.split(' ')
+        const ipBytes = connectionAddress.split('.').map(parseInt)
+        if (ipBytes.length == 4) {
+          const portNum = parseInt(port)
+          candidates.push(bytesToStr([...ipBytes, portNum & 0xff0000, portNum & 0x00ff00, portNum & 0x0000ff]))
+        }
+        break
+    }
+  }
+
+  return type.charAt(0) +
+    fingerprint +
+    bytesToStr(candidates.length)  +
+    candidates +
+    ufrag +
+    delimiter +
+    pwd
+}
---
+++ src/util.ts
@@ -0,0 +1,18 @@
+/** Char used to separate non-fixed width parts. */
+export const delimiter = ','
+
+/**
+ * Converts an array of bytes to a 'human friendly' string.
+ * 
+ * The range of 0x00 to 0xFF will be:
+ * ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿĀāĂăĄąĆćĈĉĊċČčĎďĐđĒ
+ * ēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħĨĩĪīĬĭĮįİıĲĳĴĵĶķĸĹĺĻļĽľĿŀŁłŃńŅņŇňŉŊŋŌōŎŏŐőŒœŔŕŖŗŘřŚśŜŝŞşŠšŢţŤť
+ * ŦŧŨũŪūŬŭŮůŰűŲųŴŵŶŷŸŹźŻżŽžſƀƁƂƃƄƅƆƇƈƉƊƋƌƍƎƏƐƑƒƓƔƕƖƗƘƙƚƛƜƝƞƟƠơƢƣƤƥƦƧƨƩƪƫƬƭƮƯưƱƲƳƴƵƶƷƸƹƺƻƼƽƾƿ
+ */
+export const bytesToStr = (bytes: number | number[]): string =>
+  typeof bytes == 'number'
+    ? bytesToStr([bytes])
+    : String.fromCharCode(...bytes.map(byte => byte + 192))
+
+export const strToBytes = (str: string): number[] =>
+    str.split('').map(char => char.charCodeAt(0))
